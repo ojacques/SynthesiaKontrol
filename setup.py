ef36a101e2a5097e91860b682681385693ab95b4
Olivier Jacques <ojacques2@gmail.com>
2018-04-10 23:28:53 +0200

    Create setup script for cx_freeze

---
+++ setup.py
@@ -0,0 +1,17 @@
+from cx_Freeze import setup, Executable
+   
+executables = [
+        Executable("SynthesiaKontrol.py")
+]
+   
+buildOptions = dict(
+        includes = ["mido.backends.rtmidi"]
+)
+   
+setup(
+    name = "SynthesiaKontrol",
+    version = "1.0",
+    description = "Use Komplete Kontrol Keyboard lights in Synthesia",
+    options = dict(build_exe = buildOptions),
+    executables = executables
+)
